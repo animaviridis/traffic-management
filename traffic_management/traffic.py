7cde30cf7ec12857cbab01c4af298a7862b356e4
mysh <mysh995@gmail.com>
2019-09-28 01:31:52 +0100

    [Traffic] Acceleration after switching priority

--- traffic_management/traffic.py
+++ traffic_management/traffic.py
@@ -3,14 +3,16 @@ class TrafficProperties(object):
     PRIORITY_BASE_DURATION = 1
     PRIORITY_EXT_DURATION = 0.5
     JUNCTION_FLOW = 200  # flow per minute through the junction
+    ACCELERATION_TIME = 0.25  # time before reaching the nominal junction flow
 
     @staticmethod
-    def get_outflow(time):
-        return int(TrafficProperties.JUNCTION_FLOW * time)
+    def get_outflow(time, accelerating=False):
+        delay = TrafficProperties.ACCELERATION_TIME / 2 if accelerating else 0
+        return int(TrafficProperties.JUNCTION_FLOW * (time - delay))
 
     @staticmethod
     def get_base_outflow():
-        return TrafficProperties.get_outflow(TrafficProperties.PRIORITY_BASE_DURATION)
+        return TrafficProperties.get_outflow(TrafficProperties.PRIORITY_BASE_DURATION, accelerating=True)
 
     @staticmethod
     def get_ext_outflow():
